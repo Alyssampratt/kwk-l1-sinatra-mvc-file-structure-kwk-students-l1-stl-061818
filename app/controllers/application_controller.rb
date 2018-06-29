2c60acc99b38d72a01bf3e1de495ef2090db8b9b
Alyssampratt <magoopratt@gmail.com>
2018-06-29 18:09:38 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
+require './config/environment'
+require './app/models/model'
+
+
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -5,7 +9,17 @@ class ApplicationController < Sinatra::Base
   	set :public_dir, "public"
   end
 
-  get "/" do
-  	erb :index
+  get '/' do #defult home page
+     erb :index
+  end 
+  	
+  post '/' do #we clicked the button now what
+    @the_fortune=get_fortune #method get_fortune is connected to the instance variable @the_fortune
+    @the_user=params[:user]
+    return erb :results
   end
+   
+  	get "/newpage" do
+      erb :newpage
+end
 end
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,7 @@
-class Model
-  # Replace with Dog clas
+def get_fortune
+
+fortune_array=["you will eat bok choy","you will drink 20 core waters in a week","you will fall in a hole in two days","you will lose your left shoe","you will get a nosebleed on the metro","you will come to class with wilted bok choy"]
+return fortune_array.sample
 end
+
+# puts get_fortune
